74556d4a4d2224d27b93f5442607067bfffb6789
DanGrew <danielanthonygrew@gmail.com>
2016-01-15 20:09:49 +0000

    TIME KEEPER:
    Implemented basic time keeping mechanism for polling for new jobs.

---
+++ JenkinsTestTracker/src/synchronisation/time/TimeKeeper.java
@@ -0,0 +1,62 @@
+/*
+ * ----------------------------------------
+ *          Jenkins Test Tracker
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package synchronisation.time;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import api.handling.JenkinsFetcher;
+
+/**
+ * The {@link TimeKeeper} is responsible for polling for new jobs at regular
+ * intervals.
+ */
+public class TimeKeeper {
+
+   private JenkinsFetcher fetcher;
+   private Timer timer;
+   private long interval;
+   
+   /**
+    * Constructs a new {@link TimeKeeper}.
+    * @param fetcher the {@link JenkinsFetcher} to use to fetch data.
+    * @param interval the interval of the polling.
+    */
+   public TimeKeeper( JenkinsFetcher fetcher, long interval ) {
+      this.fetcher = fetcher;
+      this.interval = interval;
+      schedule();
+   }//End Constructor
+   
+   /**
+    * Method to schedule the repeating {@link TimerTask}.
+    */
+   private void schedule(){
+      if ( timer != null ) timer.cancel();
+
+      TimerTask task = new TimerTask() {
+         @Override public void run() {
+            fetcher.fetchJobs();
+         }
+      };
+      timer = new Timer();
+      timer.schedule( task, 0, interval );
+   }//End Method
+
+   /**
+    * Method to set the interval to poll at. This will cancel the current and 
+    * start a fresh, causing an immediate poll.
+    * @param newInterval the new interval to poll at.
+    */
+   public void setInterval( long newInterval ) {
+      this.interval = newInterval;
+      schedule();
+   }//End Method
+
+}//End Class
---
+++ JenkinsTestTracker/test/synchronisation/time/TimeKeeperTest.java
@@ -0,0 +1,54 @@
+/*
+ * ----------------------------------------
+ *          Jenkins Test Tracker
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package synchronisation.time;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import api.handling.JenkinsFetcher;
+
+/**
+ * {@link TimeKeeper} test.
+ */
+public class TimeKeeperTest {
+   
+   private JenkinsFetcher fetcher;
+   
+   @Before public void initialiseSystemUnderTest(){
+      fetcher = Mockito.mock( JenkinsFetcher.class );
+   }//End Method
+   
+   @Test public void shouldRecordSomeInteractionsWhenRunQuickly() throws InterruptedException{
+      new TimeKeeper( fetcher, 1 );
+      Mockito.verify( fetcher, Mockito.atLeast( 1 ) ).fetchJobs();;
+      Thread.sleep( 10 );
+      Mockito.verify( fetcher, Mockito.atLeast( 8 ) ).fetchJobs();
+   }//End Method
+   
+   @Test public void shouldRunTaskAtLeastOnceBeforeAndOnceOnInterval() throws InterruptedException{
+      new TimeKeeper( fetcher, 1000 );
+      Thread.sleep( 1 );
+      Mockito.verify( fetcher, Mockito.times( 1 ) ).fetchJobs();;
+      Thread.sleep( 1000 );
+      Mockito.verify( fetcher, Mockito.times( 2 ) ).fetchJobs();
+   }//End Method
+   
+   @Test public void shouldRecordSomeInteractionsWhenIntervalIsChanged() throws InterruptedException{
+      TimeKeeper systemUnderTest = new TimeKeeper( fetcher, 1000 );
+      Mockito.verify( fetcher, Mockito.times( 1 ) ).fetchJobs();
+      systemUnderTest.setInterval( 2000 );
+      Thread.sleep( 1 );
+      Mockito.verify( fetcher, Mockito.times( 2 ) ).fetchJobs();
+      systemUnderTest.setInterval( 1 );
+      Thread.sleep( 10 );
+      Mockito.verify( fetcher, Mockito.atLeast( 10 ) ).fetchJobs();
+   }//End Method
+
+}//End Class
