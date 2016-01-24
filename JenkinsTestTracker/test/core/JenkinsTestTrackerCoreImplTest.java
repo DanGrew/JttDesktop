b9fc805aebeb18f6926e09f0d6e79fb85bbb00af
DanGrew <danielanthonygrew@gmail.com>
2016-01-24 09:39:02 +0000

    JENKINS TEST TRACKER:
    Added missing test case for multiple job changes.

--- JenkinsTestTracker/test/core/JenkinsTestTrackerCoreImplTest.java
+++ JenkinsTestTracker/test/core/JenkinsTestTrackerCoreImplTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mockito;
 import api.handling.BuildState;
 import api.sources.ExternalApi;
 import graphics.JavaFxInitializer;
+import model.jobs.JenkinsJob;
 import model.tests.TestResultStatus;
 import storage.database.JenkinsDatabase;
 import synchronisation.time.TimeKeeper;
@@ -187,6 +188,100 @@ public class JenkinsTestTrackerCoreImplTest {
       Assert.assertFalse( database.hasNoJenkinsJobs() );
       Assert.assertFalse( database.hasNoTestClasses() );
       Assert.assertEquals( 1, database.jenkinsJobs().size() );
+      Assert.assertEquals( 36, database.testClasses().size() );
+      Assert.assertEquals( TestResultStatus.FAILED, database.testClasses().get( 0 ).testCasesList().get( 1 ).statusProperty().get() );
+   }//End Method
+   
+   @Test public void shouldBringInMultipleJobsAndKeepThemUpdated(){
+      final ExternalApi api = Mockito.mock( ExternalApi.class );
+      JenkinsTestTrackerCoreImpl core = new JenkinsTestTrackerCoreImpl( api );
+      JenkinsDatabase database = core.getDatabase();
+      TimeKeeper timeKeeper = core.getTimeKeeper();
+      
+      Assert.assertTrue( database.hasNoJenkinsJobs() );
+      Assert.assertTrue( database.hasNoTestClasses() );
+      
+      //Pull in the only JenkinsJob...
+      String response = TestCommon.readFileIntoString( getClass(), "multiple-jobs.json" );
+      Mockito.when( api.getJobsList() ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...and prove that nothing else changes, and the job is initially built...
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertTrue( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
+      Assert.assertEquals( BuildState.Built, database.jenkinsJobs().get( 0 ).buildStateProperty().get() );
+      
+      final JenkinsJob first = database.jenkinsJobs().get( 0 );
+      final JenkinsJob second = database.jenkinsJobs().get( 1 );
+      
+      //...then simulate the building state...
+      response = TestCommon.readFileIntoString( getClass(), "building-state.json" );
+      Mockito.when( api.getLastBuildBuildingState( first ) ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...and prove the job starts building...
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertTrue( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
+      Assert.assertEquals( BuildState.Building, first.buildStateProperty().get() );
+      
+      //...finish the build and provide test results...
+      response = TestCommon.readFileIntoString( getClass(), "built-state.json" );
+      Mockito.when( api.getLastBuildBuildingState( first ) ).thenReturn( response );
+      response = TestCommon.readFileIntoString( getClass(), "all-passing-tests.json" );
+      Mockito.when( api.getLatestTestResults( first ) ).thenReturn( response );
+      response = TestCommon.readFileIntoString( getClass(), "job-details-22.json" );
+      Mockito.when( api.getLastBuildJobDetails( first ) ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...prove the build is complete and test results are parsed...
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertFalse( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
+      Assert.assertEquals( 36, database.testClasses().size() );
+      Assert.assertEquals( BuildState.Built, database.jenkinsJobs().get( 0 ).buildStateProperty().get() );
+      Assert.assertEquals( TestResultStatus.PASSED, database.testClasses().get( 0 ).testCasesList().get( 1 ).statusProperty().get() );
+      
+      //...then provide a new build with some failures...
+      response = TestCommon.readFileIntoString( getClass(), "some-failures-tests.json" );
+      Mockito.when( api.getLatestTestResults( second ) ).thenReturn( response );
+      response = TestCommon.readFileIntoString( getClass(), "job-details-23.json" );
+      Mockito.when( api.getLastBuildJobDetails( second ) ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...and prove the failures are picked up...
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertFalse( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
+      Assert.assertEquals( 36, database.testClasses().size() );
+      Assert.assertEquals( TestResultStatus.FAILED, database.testClasses().get( 0 ).testCasesList().get( 1 ).statusProperty().get() );
+      
+      //...then pass all the tests in a further build...
+      response = TestCommon.readFileIntoString( getClass(), "all-passing-tests.json" );
+      Mockito.when( api.getLatestTestResults( first ) ).thenReturn( response );
+      response = TestCommon.readFileIntoString( getClass(), "job-details-24.json" );
+      Mockito.when( api.getLastBuildJobDetails( first ) ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...and prove that they are picked up...
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertFalse( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
+      Assert.assertEquals( 36, database.testClasses().size() );
+      Assert.assertEquals( TestResultStatus.PASSED, database.testClasses().get( 0 ).testCasesList().get( 1 ).statusProperty().get() );
+      
+      //...finally fail a large number of tests...
+      response = TestCommon.readFileIntoString( getClass(), "large-failures-tests.json" );
+      Mockito.when( api.getLatestTestResults( second ) ).thenReturn( response );
+      response = TestCommon.readFileIntoString( getClass(), "job-details-25.json" );
+      Mockito.when( api.getLastBuildJobDetails( second ) ).thenReturn( response );
+      timeKeeper.poll();
+      
+      //...and prove that they are picked up.
+      Assert.assertFalse( database.hasNoJenkinsJobs() );
+      Assert.assertFalse( database.hasNoTestClasses() );
+      Assert.assertEquals( 2, database.jenkinsJobs().size() );
       Assert.assertEquals( 36, database.testClasses().size() );
       Assert.assertEquals( TestResultStatus.FAILED, database.testClasses().get( 0 ).testCasesList().get( 1 ).statusProperty().get() );
    }//End Method
