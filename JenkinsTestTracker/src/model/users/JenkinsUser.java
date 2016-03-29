7f9c390b7f6d2ebfaccb9212ea62616e32b0d9fe
DanGrew <danielanthonygrew@gmail.com>
2016-03-29 20:59:56 +0100

    JENKINS USER:
    Implemented basic jenkins user for integrating with the detailed job
    description.

---
+++ JenkinsTestTracker/src/model/users/JenkinsUser.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------
+ *          Jenkins Test Tracker
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package model.users;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * The {@link JenkinsUser} represents any user or culprit in jenkins.
+ */
+public interface JenkinsUser {
+
+   /**
+    * Provides the {@link StringProperty} of the name of the user.
+    * @return the {@link StringProperty}.
+    */
+   public StringProperty nameProperty();
+
+}//End Interface
