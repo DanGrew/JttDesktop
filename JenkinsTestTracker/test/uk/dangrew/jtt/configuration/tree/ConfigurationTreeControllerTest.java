0997cf0ce30f4418ff14f19ab77b77d49c8b3120
DanGrew <danielanthonygrew@gmail.com>
2016-06-24 08:03:59 +0100

    ISSUE#120 REDESIGN CONFIG PANELS:
    Fixed silly issue where mockito annotations not called.

--- JenkinsTestTracker/test/uk/dangrew/jtt/configuration/tree/ConfigurationTreeControllerTest.java
+++ JenkinsTestTracker/test/uk/dangrew/jtt/configuration/tree/ConfigurationTreeControllerTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.verify;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -30,6 +31,7 @@ public class ConfigurationTreeControllerTest {
    
    @Before public void initialiseSystemUnderTest(){
       JavaFxInitializer.startPlatform();
+      MockitoAnnotations.initMocks( this );
       systemUnderTest = new ConfigurationTreeController( content );
    }//End Method
    
