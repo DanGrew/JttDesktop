02c48a0221d244138171f3ddc553bd970321df3d
DanGrew <danielanthonygrew@gmail.com>
2016-06-25 10:14:26 +0100

    ISSUE#128 DUAL CONFIG PANEL:
    Added graphical panel for configuring the properties and added tree item
    for integration with the tree when ready.

---
+++ JenkinsTestTracker/src/uk/dangrew/jtt/buildwall/configuration/tree/item/DualPropertiesTreeItem.java
@@ -0,0 +1,52 @@
+/*
+ * ----------------------------------------
+ *          Jenkins Test Tracker
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package uk.dangrew.jtt.buildwall.configuration.tree.item;
+
+import uk.dangrew.jtt.buildwall.configuration.components.DualPropertiesPanel;
+import uk.dangrew.jtt.buildwall.configuration.properties.DualConfiguration;
+import uk.dangrew.jtt.configuration.item.SimpleConfigurationItem;
+import uk.dangrew.jtt.configuration.item.SimpleConfigurationTitle;
+import uk.dangrew.jtt.configuration.tree.ConfigurationTreeController;
+
+/**
+ * The {@link DualPropertiesTreeItem} provides the configuration items for the properties related
+ * specifically to the {@link uk.dangrew.jtt.buildwall.dual.DualBuildWallDisplayImpl}.
+ */
+public class DualPropertiesTreeItem extends SimpleConfigurationItem {
+
+   static final String NAME = "Dual Properties";
+   static final String TITLE = "Configuring the Dual Wall properties";
+   static final String DESCRIPTION = 
+            "The following properties apply to the dual wall as a whole and control"
+            + "how it is displayed.";
+   
+   private final DualConfiguration configuration;
+   
+   /**
+    * Constructs a new {@link DualPropertiesTreeItem}.
+    * @param controller the {@link ConfigurationTreeController} for controlling the configuration.
+    * @param configuration the {@link DualConfiguration} associated.
+    */
+   public DualPropertiesTreeItem( ConfigurationTreeController controller, DualConfiguration configuration ) {
+      super( 
+               NAME, 
+               new SimpleConfigurationTitle( TITLE, DESCRIPTION ),
+               controller, 
+               new DualPropertiesPanel( configuration ) 
+      );
+      this.configuration = configuration;
+   }//End Constructor
+   
+   /**
+    * {@inheritDoc}
+    */
+   @Override public boolean isAssociatedWith( Object object ) {
+      return this.configuration.equals( object );
+   }//End Method
+}//End Class
